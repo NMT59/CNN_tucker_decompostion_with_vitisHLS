b2840cd940e70c739799ffb63a16a36bb0fc4fa9
NMT59 <minhtriet592002@gmail.com>
2024-12-09 13:32:05 +0700

    Delete Dense.h

--- Dense.h
+++
@@ -1,2 +0,0 @@
-void Dense_0(float input_Dense[120],float output_Dense[84],float bias[84],float weight[10080]);
-void Dense_1(float input_Dense[84],float &output_Dense,float bias[10],float weight[840]);
