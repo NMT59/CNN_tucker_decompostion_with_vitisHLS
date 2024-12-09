473c346d8fa2d8824b84f3b8d97f3297269b949c
NMT59 <minhtriet592002@gmail.com>
2024-12-09 13:32:37 +0700

    Delete Pool.h

--- Pool.h
+++
@@ -1,3 +0,0 @@
-void Max_Pool2D_0(float input_MaxPooling[180], float output_MaxPooling[84]);
-void Max_Pool2D_1(float input_MaxPooling[192], float output_MaxPooling[80]);
-void flatten(float input_Flatten[120],float output_Flatten[120]);
